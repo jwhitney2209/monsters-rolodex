adaf12419cda61a0f2e8332aacca138e23d6ac0a
teachergg <jwhitney@bisd.us>
2023-01-22 05:55:52 -0600

    initial commit

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+import CardContainer from "../card-container/card-container.component.jsx";
+import "./card-list.styles.css";
+
+class CardList extends Component {
+  render() {
+    console.log("render from Cardlist");
+    const { monsters } = this.props;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          return <CardContainer monster={monster} />;
+        })}
+      </div>
+    );
+  }
+}
+export default CardList;
